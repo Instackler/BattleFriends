329d1c95e6e8f4c6868390b6fdeb6b2d02c646dd
Konstantin Izmestiev <izmestiev.kapr.2@yandex.ru>
2023-06-17 21:11:40 +0400

    SFML window

--- BattleFriends/src/main.cpp
+++ BattleFriends/src/main.cpp
@@ -1,8 +1,27 @@
-#include <iostream>
-
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
-	std::cin.get();
+	sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
+	//window.setFramerateLimit(60);
+	window.setVerticalSyncEnabled(true);
+
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
 }
