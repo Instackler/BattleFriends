8eef26a54da781751288aa2e35b20c59861381d7
Konstantin Izmestiev <izmestiev.kapr.2@yandex.ru>
2023-07-09 04:57:24 +0400

    uncomment for stackoverflow, works without errors in Release mode

--- BattleFriends/src/minimap.cpp
+++ BattleFriends/src/minimap.cpp
@@ -4,17 +4,21 @@
 
 
 static sf::RenderTexture texture;
+//static std::shared_ptr<sf::RenderTexture> texture;
 static sf::Sprite sprite;
 static sf::RectangleShape background(sf::Vector2f(MAP_WIDTH, MAP_HEIGHT));
 
 void minimap::init()
 {
+	//texture = std::make_shared<sf::RenderTexture>();
+	
 	texture.create(5000, 5000); //add error checking
 	texture.setSmooth(true);
 	sprite.setTexture(texture.getTexture());
 	sprite.setPosition(1400.f, 20.f);
 	sprite.setScale(0.06f, 0.06f);
 	sprite.setColor(sf::Color(255, 255, 255, 100));
+	
 }
 
 void minimap::update()
